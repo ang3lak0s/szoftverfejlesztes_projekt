a341a6f1dbf9f415ea0f25b5a782c92e2a85bee4
berenyib <berenyibenedek@gmail.com>
2025-10-15 11:48:55 +0200

    User class, UserRepo interface, UserService class

--- src/main/java/com/example/szoftverfejlesztes_projekt/SzoftverfejlesztesProjektApplication.java
+++ src/main/java/com/example/szoftverfejlesztes_projekt/SzoftverfejlesztesProjektApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
 public class SzoftverfejlesztesProjektApplication {
---
+++ src/main/java/com/example/szoftverfejlesztes_projekt/UserService.java
@@ -0,0 +1,29 @@
+package com.example.szoftverfejlesztes_projekt;
+
+import com.example.szoftverfejlesztes_projekt.User;
+import com.example.szoftverfejlesztes_projekt.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
