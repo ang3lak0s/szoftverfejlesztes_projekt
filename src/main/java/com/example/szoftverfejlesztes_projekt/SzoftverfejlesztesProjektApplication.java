8645ef5984ef232313bee147b6242fc3ce37ee29
ang3lak0s <angelakos20050511@gmail.com>
2025-11-03 14:03:25 +0100

    First backend commit

--- src/main/java/com/example/szoftverfejlesztes_projekt/SzoftverfejlesztesProjektApplication.java
+++ src/main/java/com/example/szoftverfejlesztes_projekt/SzoftverfejlesztesProjektApplication.java
@@ -2,24 +2,56 @@ package com.example.szoftverfejlesztes_projekt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/main")
 public class SzoftverfejlesztesProjektApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(SzoftverfejlesztesProjektApplication.class, args);
     }
 
-    @GetMapping("/hello")
+    public static String test()
+    {
+        return String.format("This is a test.");
+    }
+
     public String hello(@RequestParam(value = "myName", defaultValue = "World") String name)
     {
         return String.format("Hello %s!", name);
     }
+
+    public String createBand()
+    {
+        Band band1 = new Band();
+        band1.setBandName("HOC");
+        band1.setBandId(67L);
+        band1.setEmail("hoc@kurvaanyad.com");
+        return String.format("Created the band %s with id %d", band1.getBandName(), band1.getBandId());
+    }
+
+    @GetMapping
+    public String mainHandler()
+    {
+        String bandTest = createBand();
+        String testResult = test();
+
+        String helloResult = hello("test");
+
+        return String.format(
+                "<h1>Main Page Content</h1>" +
+                        "<p>Test Result: <strong>%s</strong></p>" +
+                        "<p>Hello Result: <strong>%s</strong></p>" +
+                        "<p>BandTest Result: <strong>%s</strong></p>",
+                testResult,
+                helloResult,
+                bandTest
+        );
+    }
+
 
 }
