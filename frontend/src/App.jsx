07ae5ab02a78e1a6da761174a92c0a03d5447895
nagylaci11a <nagylaci11a@gmail.com>
2025-11-29 21:23:21 +0100

    Frontend 1: a Kezdet

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Zenekarok és Helyszínek – React frontend</h1>
+      <p>Két kezem összeteszem, hogy működjön, vagy személyesen megyek el indiába verekedni</p>
+    </div>
+  );
+}
+
+export default App;
